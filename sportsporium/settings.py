c55e842cec9a81a34259743cd779ede74ee3b89f
Ranj247 <ranjeet.sadioura@gmail.com>
2021-12-09 23:32:54 +0000

    Removed secret key and set debug

--- sportsporium/settings.py
+++ sportsporium/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v-g@!4gk$fr^+os@=ke7=*58%ojqyr%z*h@j8=ycvaxui_)aa'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rs87801-sportsporium.herokuapp.com', 'localhost']
 
